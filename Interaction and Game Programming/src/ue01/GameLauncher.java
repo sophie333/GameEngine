4844fb280b2207e99341093a56b61f7bac7d967c
sophie.amelin <42768125+sophie333@users.noreply.github.com>
2018-10-10 13:46:11 +0200

    updated names

--- Interaction and Game Programming/src/ue01/GameLauncher.java
+++ Interaction and Game Programming/src/ue01/GameLauncher.java
@@ -4,7 +4,7 @@ import at.fhooe.mtd.sgl.app.Java2dApplication;
 import at.fhooe.mtd.sgl.app.Java2dApplicationConfig;
 import at.fhooe.mtd.sgl.graphics.GfxConfigurator;
 
-public class HelloLauncher {
+public class GameLauncher {
 
 	public static void main(String[] args) {
 		GfxConfigurator gfxc = new GfxConfigurator();
@@ -12,7 +12,7 @@ public class HelloLauncher {
 		gfxc.runDialog();
 		
 		Java2dApplicationConfig config = Java2dApplicationConfig.create(gfxc);
-		config.title = "Hello Sgl";
+		config.title = "Asteroids";
 		new Java2dApplication(config, new HelloWorld());
 	}
 
